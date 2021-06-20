8fc7c4e6a3d4207a983f7e4b1b1ecc64ba4e714c
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2021-06-20 22:41:13 +0300

    Main

---
+++ src/com/chess/engine/classic/ChessPlayer.java
@@ -0,0 +1,10 @@
+package com.chess;
+// +Simay Martinova
+import com.chess.gui.Table;
+
+public class ChessPlayer {
+
+    public static void main(final String[] args) throws Exception {
+        Table.get().show();
+    }
+}
